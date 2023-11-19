d2e8a0c7e615331ba91101ce97ac0d1be88cb71b
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-20 02:21:07 +0530

    starting the server

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+// underlying storage (in memory, on disk, s3, anything)
+// server (http, tcp)
 
 func main() {
-	fmt.Println("Message queue in golang")
+	cfg := &Config{
+		ListenAddr: ":3000",
+		StoreProducerFunc: func() Storer {
+			return NewMemoryStore()
+		},
+	}
+	s, err := NewServer(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	s.Start()
 }
