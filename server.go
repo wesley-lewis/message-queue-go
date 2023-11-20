6004ef4e6c4d4c3b85f9cd61f46b77c44e9f93db
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-20 12:15:41 +0530

    listening for topics

--- server.go
+++ server.go
@@ -2,7 +2,14 @@ package main
 
 import (
 	"fmt"
+
+	"golang.org/x/exp/slog"
 )
+
+type Message struct {
+	Topic string 
+	Data []byte
+}
 
 type Config struct {
 	ListenAddr        string
@@ -14,42 +21,62 @@ type Server struct {
 	topics map[string]Storer
 	consumers []Consumer
 	producers []Producer
+	producech chan Message
 	quitch chan struct{}
 }
 
 func NewServer(cfg *Config) (*Server, error) {
+	producech := make(chan Message)
 	return &Server{
 		Config: cfg,
 		topics: make(map[string]Storer),
 		quitch: make(chan struct{}),
-		producers: []Producer{NewHTTPProducer(cfg.ListenAddr)},
+		producech: producech,
+		producers: []Producer{NewHTTPProducer(cfg.ListenAddr, producech)},
 	}, nil
 }
 
 func (s *Server) Start() {
-	for _, consumer := range s.consumers {
-		if err := consumer.Start(); err != nil {
-			fmt.Println(err)
-		}
-	}
+	// for _, consumer := range s.consumers {
+	// 	if err := consumer.Start(); err != nil {
+	// 		fmt.Println(err)
+	// 	}
+	// }
 
 	for _, producer := range s.producers {
-		if err := producer.Start(); err != nil {
-			fmt.Println(err)
-		}
+		go func(p Producer) {
+			if err := p.Start(); err != nil {
+				fmt.Println(err)
+			}
+		}(producer)
 	}
-	<- s.quitch
+	s.loop()
 	// http.ListenAndServe(s.Config.ListenAddr, s)
 }
 
-func(s *Server) publish() error {
+func(s *Server) loop() {
+	for {
+		select {
+		case <- s.quitch:
+			return
+		case msg := <- s.producech:
+			fmt.Println("Produced: ", msg)
+			if err := s.publish(msg); err != nil {
+				slog.Error("failed to publish", err)
+			}
+		}
+	}
+}
+
+func(s *Server) publish(msg Message) error {
+	s.createTopicIfNotExist(msg.Topic)
+
 	return nil
 }
 
-func (s *Server) createTopic(name string) bool {
-	if _, ok := s.topics[name]; !ok {
-		s.topics[name] = s.Config.StoreProducerFunc()
-		return true
+func (s *Server) createTopicIfNotExist(topic string)  {
+	if _, ok := s.topics[topic]; !ok {
+		s.topics[topic] = s.Config.StoreProducerFunc()
+		slog.Info("created new topic", "topic", topic)
 	}
-	return false
 }
