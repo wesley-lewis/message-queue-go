503f43dc9cf6ca5adc20a34ca987f54a00799a39
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-20 13:08:31 +0530

    removed test

--- storage_test.go
+++ storage_test.go
@@ -8,23 +8,5 @@ import (
 )
 
 func TestStorage(t *testing.T) {
-	s := NewMemoryStore()
-	var err error
-	for i := 0; i < 100; i++ {
-		key := fmt.Sprintf("foobarbaz_%d", i)
-		_, err = s.Push([]byte(key))
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	for i := 0; i < 100; i++ {
-		data, err := s.Fetch(int32(i))
-		if err != nil {
-			panic(err)
-		}
-
-		value := fmt.Sprintf("foobarbaz_%d", i)
-		assert.Equal(t, data, []byte(value))
-	}
+}
 }
