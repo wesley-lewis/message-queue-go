4602edf3ab249d3652e61b45a0d1429bc65df9e8
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-23 01:20:57 +0530

    WSConsumer created

--- consumer.go
+++ consumer.go
@@ -1,11 +1,47 @@
 package main
 
-import(
+import (
+	"fmt"
+	"net/http"
+
 	"github.com/gorilla/websocket"
+	"golang.org/x/exp/slog"
 )
+
+var upgrader = websocket.Upgrader{}
 
 type Consumer interface {
 	Start() error
+}
+
+type WSConsumer struct {
+	ListenAddr string 
+	server *Server
+}
+
+func NewWSConsumer(listenAddr string, s *Server ) *WSConsumer {
+	return &WSConsumer{
+		ListenAddr: listenAddr,
+		server: s,
+	}
+}
+
+func (ws *WSConsumer) Start() error {
+	slog.Info("websocket consumer started", "port", ws.ListenAddr)
+	http.ListenAndServe(":4000", ws)
+	return nil
+}
+
+func(ws *WSConsumer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+		return 
+	}
+
+	p := NewWSPPeer(conn)
+	ws.server.AddConn(p)
+	fmt.Println(conn)
 }
 
 func foo() {
