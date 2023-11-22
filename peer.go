d49f9a3f7cf16cd5467a00b12e2338639de2213a
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-23 02:25:36 +0530

    reading bytes from peer

--- peer.go
+++ peer.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/gorilla/websocket"
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"golang.org/x/exp/slog"
+)
 
 type Peer interface {
 	Send([]byte) error
@@ -8,13 +12,34 @@ type Peer interface {
 
 type WSPeer struct {
 	conn *websocket.Conn
-	
 }
 
-func NewWSPPeer(conn *websocket.Conn) *WSPeer {
-	return &WSPeer {
+func NewWSPPeer(conn *websocket.Conn) Peer {
+	p :=  &WSPeer {
 		conn: conn,
 	}
+	go p.readLoop()
+	return p
+}
+
+func(p *WSPeer) readLoop() {
+	var msg WSMessage 
+
+	for {
+		if err := p.conn.ReadJSON(&msg); err != nil {
+			slog.Error("WS peer readJSON error", "error", err)
+			return 
+		}
+		if err := p.handleMessage(msg); err != nil {
+			slog.Error("WS peer read error", "error", err)
+			return 
+		}
+	}
+}
+
+func (p *WSPeer) handleMessage(msg WSMessage) error {
+	fmt.Printf("handling msg -> %+v\n", msg)
+	return nil
 }
 
 func(p *WSPeer) Send(b []byte) (error) {
